946194820e0867eb5691290650134254554f1460
satazero <satazero@pop-os.localdomain>
2022-05-01 15:56:29 -0500

    Added viewing of transactions

---
+++ RecentTransactions.php
@@ -0,0 +1,30 @@
+<h1>Last 50 Transactions:</h1>
+<?php 
+
+$servername = "localhost";
+$username = "test";
+$password = "P@ssword1!";
+$dbname = "647Project";
+
+$conn = new mysqli( $servername, $username, $password, $dbname );
+if ( $conn->connect_error )
+{
+    die("Connection failed: " . $conn->connect_error );
+}
+
+$query = "SELECT PRODUCT.sku AS sku, PRODUCT.name AS product_name, cid, CUSTOMER.name AS customer_name, quantity FROM TRANSACTIONS LEFT JOIN CUSTOMER ON TRANSACTIONS.customer = CUSTOMER.cid LEFT JOIN PRODUCT ON TRANSACTIONS.sku = PRODUCT.SKU;";
+$result = $conn->query( $query );
+
+if ( $result->num_rows > 0 )
+{
+    echo "<table><th>SKU</th><th>Product</th><th>CID</th><th>Customer</th></tr>";
+    while ( $row = $result->fetch_assoc() )
+    {
+        echo "<tr><td>" . $row["sku"] . "</td><td>" . $row["product_name"] . "</td><td>" . $row["cid"] . "</td><td>" . $row["customer_name"] . "</td></tr>";
+        //echo "<tr><td>" . $row["SKU"] . "</td><td>" . $row["name"] . "</td><td>" . $row["department_id"] . "</td><td>" . $row["quantity"] . "</td><tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No results";
+}
+?>
